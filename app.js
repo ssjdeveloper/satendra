a35f5c00908930c4f2502de3e9fb4e213e1da08e
Satendra <satendrasinghjamhoriya2002@gmail.com>
2022-12-16 04:01:59 +0530

    complete web site

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-links");
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+
+var sidemenus = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenus.style.right="0";
+}
+
+function closemenu(){
+    sidemenus.style.right="-200px";
+}
